ca77521a881f4a0006fbdcc83b6b1c2031709fee
Emillia <hentaimasterpad@gmail.com>
2022-09-03 22:27:21 +0200

    it's the wrong way around ;(

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('v2/api')
+                ->prefix('api/v2')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
