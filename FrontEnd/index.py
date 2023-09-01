d53e6aeaa27db7da8f3cc0b1fce722b794af48dd
Emillia <hentaimasterpad@gmail.com>
2023-09-01 18:10:05 +0200

    FrontEnd little bit done
    FrontEnd and BackEnd will be added in separate commits but so far this will be the frontend

---
+++ FrontEnd/index.py
@@ -0,0 +1,19 @@
+import sys
+import json
+import os
+
+from jinja2_static import Builder
+
+build = Builder()
+argument = sys.argv[1] if len(sys.argv) > 1 else None
+
+with open("./data.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+
+build.generate(
+    debug=False if argument == "build" else True,
+    URL="https://api.computerfreaker.pw", data=data,
+    minify_html=True
+)
+Í
---
+++ FrontEnd/static/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("scroll", () => {
+  // Hide arrow button when scrolling down the page
+  const scroll_y = window.scrollY
+})
+
